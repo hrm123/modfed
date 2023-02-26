22e234efa1e1d292cc8103c8d30540674ab890ad
rammohanh <hrammohan@gmail.com>
2023-02-26 10:29:04 -0800

    code

---
+++ ng-resume-chrono/projects/vendors/src/lib/vendors.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-vendors',
+  template: `
+    <p>
+      vendors works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class VendorsComponent {
+
+}
